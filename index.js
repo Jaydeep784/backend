2e2843854dd27c602ef0c8f672263a51ef9632db
Jaydeep Jadhav <jaydeepjadhav784@gmail.com>
2024-09-03 13:21:39 +0530

    second commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const productRoute = require('./routes/Product');
 const userRoute = require('./routes/User');
 const cors = require("cors");
 const app = express();
+// app.use(cors());
 app.use(cors({
     origin : ['https://marketplace-client-nu.vercel.app'],
     methods : ['GET', 'POST'],
@@ -16,6 +17,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());    
+app.use(express.static('public'))
 
 mongoose.connect('mongodb+srv://jaydeepjadhav784:jIzY5YNhlxFtFxoj@cluster0.72qwz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => console.log("Database connected")).catch((e) => console.log(e));
 
--- routes/Product.js
+++ routes/Product.js
@@ -1,18 +1,56 @@
 const express = require("express");
 const Product = require("../models/Product");
 const router = express();
+// const upload = require("../middlewares/storage")
+// import { upload } from '../middlewares/storage';
+const multer = require("multer");
+const path = require("path");
 
-router.post("/productDetails", async (req, res) => {
-  const { sellerName, mobile, productName, productImg, productDet } = req.body;
-  const result = await Product.create({
-    sellerName,
-    mobile,
-    productName,
-    productImg,
-    productDet,
-  });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/Images");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
 
-  return res.status(201).json({msg : "Product details added successfully!", result})
+const upload = multer({
+  storage: storage,
+});
+
+router.post("/productDetails", upload.single('productImg'), async (req, res) => {
+  try {
+    // Destructure form fields
+    const { sellerName, mobile, productName, productDet } = req.body;
+    console.log(req.file);
+    const productImg = req.file ? req.file.filename : null; // Get filename from multer
+
+    // Create a new product entry
+    const result = await Product.create({
+      sellerName,
+      mobile,
+      productName,
+      productImg, // Store the image filename
+      productDet,
+    });
+
+    res.status(201).json({ msg: "Product details added successfully!", result });
+  } catch (error) {
+    res.status(500).json({ msg: "Error adding product details", error });
+  }
+});
+
+router.get("/getProducts", async (req, res) => {
+  try {
+    const result = await Product.find();
+    return res.json({ msg: "Products retrieved successfully", result });
+  } catch (error) {
+    return res.status(500).json({ msg: "Error retrieving products", error });
+  }
 });
 
 module.exports = router;
